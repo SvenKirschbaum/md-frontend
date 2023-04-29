3ff6566476dc3b4ca1c00d8eb1765da5abf027c5
Sven Kirschbaum <sven@kirschbaum.me>
2023-04-30 01:11:23 +0200

    :construction_worker: Migrate from cra to vite

--- src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-// noinspection NpmUsedModulesInstalled
-const {createProxyMiddleware} = require('http-proxy-middleware');
-
-module.exports = function (app) {
-    //Proxy to local backend
-    //app.use(createProxyMiddleware('/api', {target: 'http://localhost:8080'}));
-    //Proxy to production backend
-    app.use(createProxyMiddleware('/api', {target: 'https://www.markus-dope.de', secure: true, changeOrigin: true}));
-};
---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import {defineConfig, splitVendorChunkPlugin} from 'vite';
+import react from '@vitejs/plugin-react';
+import svgrPlugin from 'vite-plugin-svgr';
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+
+const apiHost = 'https://www.markus-dope.de';
+// const apiHost = 'http://localhost:8080';
+
+const secure = apiHost.includes('https');
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), svgrPlugin(), basicSsl(), splitVendorChunkPlugin()],
+    server: {
+        port: 3000,
+        // Required to work with oauth provider
+        https: true,
+        proxy: {
+            '/api': {
+                target: apiHost,
+                changeOrigin: secure,
+                secure,
+            }
+        },
+    },
+    preview: {
+        port: 3000
+    },
+    build: {
+        outDir: 'build'
+    },
+    resolve: {
+        // Required for react-moment: https://github.com/vitejs/vite/issues/7376
+        mainFields: []
+    }
+});
