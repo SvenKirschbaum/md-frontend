1483bdc42f8738cc0ae9ba85fdc8de108ee0ac87
Sven Kirschbaum <sven@kirschbaum.me>
2023-10-23 07:46:24 +0200

    :sparkles: Ádd Cookie Banner

--- src/App.jsx
+++ src/App.jsx
@@ -11,31 +11,43 @@ import './res/bootstrap.min.css';
 import ImportMatch from "./views/ImportMatch";
 import RoleRoute from "./components/RoleRoute";
 import MatchView from "./views/MatchView";
+import {CookieConsent} from "react-cookie-consent";
 
 function App() {
     return (
-        <ReactKeycloakProvider authClient={keycloak} LoadingComponent={<div/>} initOptions={{
-            onLoad: 'check-sso',
-            promiseType: 'native',
-            flow: 'standard',
-            pkceMethod: 'S256',
-            checkLoginIframe: false,
-            silentCheckSsoRedirectUri: window.location.origin + '/silent-sso.html'
-        }}>
-            <Router>
-                <div className={"content"}>
-                    <div className={"main-content"}>
-                        <Switch>
-                            <Route path={"/"} exact component={Home}/>
-                            <Route path={"/match/:id"} component={MatchView}/>
-                            <RoleRoute role="manager" path={"/import"} component={ImportMatch}/>
-                            <Route component={NoMatch}/>
-                        </Switch>
+        <>
+            <CookieConsent
+                location="bottom"
+                cookieSecurity={true}
+                sameSite="strict"
+                style={{background: "black"}}
+            >
+                This website uses cookies to ensure you get the best experience on our website. <a
+                className="cookielink" href="https://cookiesandyou.com/">Learn more</a>
+            </CookieConsent>
+            <ReactKeycloakProvider authClient={keycloak} LoadingComponent={<div/>} initOptions={{
+                onLoad: 'check-sso',
+                promiseType: 'native',
+                flow: 'standard',
+                pkceMethod: 'S256',
+                checkLoginIframe: false,
+                silentCheckSsoRedirectUri: window.location.origin + '/silent-sso.html'
+            }}>
+                <Router>
+                    <div className={"content"}>
+                        <div className={"main-content"}>
+                            <Switch>
+                                <Route path={"/"} exact component={Home}/>
+                                <Route path={"/match/:id"} component={MatchView}/>
+                                <RoleRoute role="manager" path={"/import"} component={ImportMatch}/>
+                                <Route component={NoMatch}/>
+                            </Switch>
+                        </div>
+                        <Footer/>
                     </div>
-                    <Footer/>
-                </div>
-            </Router>
-        </ReactKeycloakProvider>
+                </Router>
+            </ReactKeycloakProvider>
+        </>
     );
 }
 
